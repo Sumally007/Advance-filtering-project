eb780072498fc5f88e4687f66c21f71d844257b1
Sumally007 <ismailmbarouk007@gmail.com>
2026-04-10 17:23:00 +0300

    small improvement on refactoring the code

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { AiFillStar } from 'react-icons/ai'
+import { BsFillBagHeartFill } from 'react-icons/bs'
 
 const Cards = () => {
     return (
-        <div>Cards</div>
+        <section className="card">
+            <img className='card-img' src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="Shoe" />
+
+            <div className="card-details">
+                <h3 className="card-title">Shoe</h3>
+                <section className="card-reviews">
+                    <AiFillStar className='ratings-start' /><AiFillStar className='ratings-start' /><AiFillStar className='ratings-start' /><AiFillStar className='ratings-start' />
+                    <span className='total-revies'>4</span>
+                </section>
+
+                <section className="card-price">
+                    <div className="price">
+                        <del>$300</del> 200
+                    </div>
+
+                    <div className="bag">
+                        <BsFillBagHeartFill className='bag-icon' />
+                    </div>
+                </section>
+            </div>
+        </section>
     )
 }
 
