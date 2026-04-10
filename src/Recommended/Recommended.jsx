640d43f33e923a6ba15a678ec2cfc854edeed3fa
Sumally007 <ismailmbarouk007@gmail.com>
2026-04-10 14:00:06 +0300

    Recommended component done

--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import "./Recommended.css";
 
 const Recommended = () => {
-    return (
-        <div>Recommended</div>
+    return (<>
+        <div>
+            <h2 >
+                Recommended
+            </h2>
+            <div className="recommeneded-btns">
+                <button className="btns">All Products</button>
+                <button className="btns">Nike</button>
+                <button className="btns">Adidas</button>
+                <button className="btns">Puma</button>
+                <button className="btns">Vans</button>
+            </div>
+        </div>
+    </>
+
     )
 }
 
-export default Recommended
+export default Recommended;
