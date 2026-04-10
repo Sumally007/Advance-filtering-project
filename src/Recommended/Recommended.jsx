f0381705b897797b21a1bfd01128d36016beeaf4
Sumally007 <ismailmbarouk007@gmail.com>
2026-04-10 15:22:22 +0300

    sidebar category section done

--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -3,11 +3,11 @@ import "./Recommended.css";
 
 const Recommended = () => {
     return (<>
-        <div>
-            <h2 >
+        <div >
+            <h2 className='recommended-title'>
                 Recommended
             </h2>
-            <div className="recommeneded-btns">
+            <div className='recommended-flex'>
                 <button className="btns">All Products</button>
                 <button className="btns">Nike</button>
                 <button className="btns">Adidas</button>
