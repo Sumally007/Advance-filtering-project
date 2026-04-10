e33e0eb0cb580b5ac946a46a65339081d26ee03a
Sumally007 <ismailmbarouk007@gmail.com>
2026-04-10 08:14:22 +0300

    setting up the files enviroment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+
+function App() {
+    return (<>
+        <Navigation />
+        <Products />
+        <Recommended />
+    </>
+    )
+}
+
+export default App;
